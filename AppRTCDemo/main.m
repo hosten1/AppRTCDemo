46376c2cf747504c6f44f03f8b276c6a551e3ed6
hosten1 <hosten_lym@163.com>
2016-11-03 16:52:49 +0800

    AppRTCDemo 抽取自源码
    AppRTCDemo

--- AppRTCDemo/main.m
+++ AppRTCDemo/main.m
@@ -1,16 +1,20 @@
-//
-//  main.m
-//  AppRTCDemo
-//
-//  Created by VRV2 on 2016/11/3.
-//  Copyright © 2016年 Hosten. All rights reserved.
-//
+/*
+ *  Copyright 2014 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
+#import "ARDAppDelegate.h"
+
+int main(int argc, char* argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(
+                                 argc, argv, nil, NSStringFromClass([ARDAppDelegate class]));
     }
 }
