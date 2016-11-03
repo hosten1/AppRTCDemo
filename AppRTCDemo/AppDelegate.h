808482058e5a1fe9303049ba2c343e391ef7718e
hosten1 <hosten_lym@163.com>
2016-11-03 15:57:55 +0800

    Initial Commit

---
+++ AppRTCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppRTCDemo
+//
+//  Created by VRV2 on 2016/11/3.
+//  Copyright © 2016年 Hosten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppRTCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppRTCDemo
+//
+//  Created by VRV2 on 2016/11/3.
+//  Copyright © 2016年 Hosten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
